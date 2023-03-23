c80a8269148f8e90ff7c6ad7fe41e1d52f05307a
Rihab ATTIG <rihab.attig@yahoo.fr>
2023-03-23 16:21:54 +0100

    First Commit

--- src/main/java/com/DisplNumIsPalindromFibonacciSort/exemples/ExercicesStreams.java
+++ src/main/java/com/DisplNumIsPalindromFibonacciSort/exemples/ExercicesStreams.java
@@ -14,123 +14,120 @@ import java.util.stream.Stream;
 public class ExercicesStreams {
 
 	private ExercicesStreams() {
-		
+
 	}
-	
+
 	public static void displayNumbers(int limit) {
-		Stream.iterate(0, n-> n+1)
-		.limit(limit)
-		.forEach(e-> System.out.print(e + " "));
+		Stream.iterate(0, n -> n + 1).limit(limit).forEach(e -> System.out.print(e + " "));
 	}
-	
-	  // public static boolean isPalindrome(String word) {
-	  // return Stream.of(word)
-	  // .filter(w -> w.equals(new StringBuilder(word).reverse().toString()))
-	  // .count() > 0;
-	  // }
+
+	// public static boolean isPalindrome(String word) {
+	// return Stream.of(word)
+	// .filter(w -> w.equals(new StringBuilder(word).reverse().toString()))
+	// .count() > 0;
+	// }
 	public static boolean isPalindrome(String str) {
-		return IntStream.range(0, str.length() / 2)
-				.allMatch(i -> str.charAt(i) == str.charAt(str.length() - i - 1));
+		return IntStream.range(0, str.length() / 2).allMatch(i -> str.charAt(i) == str.charAt(str.length() - i - 1));
 	}
-		  // public static Optional<Integer> fibonacci(int nb) {
-	  // return Stream.iterate(new int[] { 0, 1 }, e -> new int[] { e[1], e[0] + e[1] })
-	  // .skip(--nb)
-	  // .map(e -> e[0])
-	  // .findFirst();
-	  // }
+
+	// public static Optional<Integer> fibonacci(int nb) {
+	// return Stream.iterate(new int[] { 0, 1 }, e -> new int[] { e[1], e[0] + e[1]
+	// })
+	// .skip(--nb)
+	// .map(e -> e[0])
+	// .findFirst();
+	// }
 	public static int fibonacci(int nb) {
-		return Stream.iterate(new int[] {0, 1}, e -> new int[] {e[1], e[0] + e[1]})
-				.limit(nb).reduce((a, b) -> b)
+		return Stream.iterate(new int[] { 0, 1 }, e -> new int[] { e[1], e[0] + e[1] }).limit(nb).reduce((a, b) -> b)
 				.map(e -> e[0]).orElseThrow(IllegalArgumentException::new);
 	}
-	
+
 	// public static List<Integer> sortTab(Integer[] tab) {
-	  // return Stream.of(tab).sorted().toList();
-	  // }
+	// return Stream.of(tab).sorted().toList();
+	// }
 	public static <T> List<T> sortTab(T[] tab, Comparator<T> c) {
 		return Stream.of(tab).sorted(c).collect(Collectors.toList());
 	}
 
-	  // public static void displayAlphabet() {
-	  // Stream.iterate('a', c -> ++c).limit(26).forEach(System.out::print);
-	  // }
+	// public static void displayAlphabet() {
+	// Stream.iterate('a', c -> ++c).limit(26).forEach(System.out::print);
+	// }
 
-	  public static void displayAlphabet() {
-	    IntStream.rangeClosed('a', 'z').forEach(c -> System.out.print((char) c));
-	  }
+	public static void displayAlphabet() {
+		IntStream.rangeClosed('a', 'z').forEach(c -> System.out.print((char) c));
+	}
 
-	  // public static int myAbs(int nb) {
-	  // return IntStream.of(nb).map(n -> (n < 0) ? -n : n).findFirst().getAsInt();
-	  // }
-	  public static int myAbs(int nb) {
-	    return IntStream.of(nb).map(Math::abs).findFirst().getAsInt();
-	  }
+	// public static int myAbs(int nb) {
+	// return IntStream.of(nb).map(n -> (n < 0) ? -n : n).findFirst().getAsInt();
+	// }
+	public static int myAbs(int nb) {
+		return IntStream.of(nb).map(Math::abs).findFirst().getAsInt();
+	}
 
-	  public static void displayTab(int[] tab) {
-	    Arrays.stream(tab).forEach(System.out::print);
-	  }
+	public static void displayTab(int[] tab) {
+		Arrays.stream(tab).forEach(System.out::print);
+	}
 
-	  public static int maxTab(int[] tab) {
-	    return Arrays.stream(tab).max().getAsInt();
-	  }
+	public static int maxTab(int[] tab) {
+		return Arrays.stream(tab).max().getAsInt();
+	}
 
-	  public static double mean(double... values) {
-	    return Arrays.stream(values).average().getAsDouble();
-	  }
+	public static double mean(double... values) {
+		return Arrays.stream(values).average().getAsDouble();
+	}
 
-	  public static long countVowels(String str) {
-	    return str.chars().filter(c -> "aeiouy".contains(Character.toString(c))).count();
-	  }
+	public static long countVowels(String str) {
+		return str.chars().filter(c -> "aeiouy".contains(Character.toString(c))).count();
+	}
 
-	  public static long countConsonants(String str) {
-	    return str.chars().filter(c -> Pattern.matches("[zrtpqsdfghjklmwxcvbn]", Character.toString(c))).count();
-	  }
+	public static long countConsonants(String str) {
+		return str.chars().filter(c -> Pattern.matches("[zrtpqsdfghjklmwxcvbn]", Character.toString(c))).count();
+	}
 
-	  public static void displayMultTables(int n) {
-	    IntStream.rangeClosed(1, n).forEach(u -> {
-	      IntStream.rangeClosed(1, 10).forEach(v -> System.out.printf("%4d", u * v));
-	      System.out.println();
-	    });
-	  }
+	public static void displayMultTables(int n) {
+		IntStream.rangeClosed(1, n).forEach(u -> {
+			IntStream.rangeClosed(1, 10).forEach(v -> System.out.printf("%4d", u * v));
+			System.out.println();
+		});
+	}
 
-	  public static boolean containsUpperCase(String str) {
-	    return str.chars().anyMatch(Character::isUpperCase);
-	  }
+	public static boolean containsUpperCase(String str) {
+		return str.chars().anyMatch(Character::isUpperCase);
+	}
 
-	  
-	  public static long factorial(int nb) {
-	    return IntStream.rangeClosed(2, nb).reduce(1, (x, y) -> x * y);
-	  }
+	public static long factorial(int nb) {
+		return IntStream.rangeClosed(2, nb).reduce(1, (x, y) -> x * y);
+	}
 
+	public static <T> List<T> filterList(List<T> list, Predicate<T> p) {
+		return list.stream().filter(p).collect(Collectors.toList());
+	}
 
+	// public static Integer sumSquare(int nb) {
+	// return IntStream.rangeClosed(0, nb).filter(e -> e % 2 == 0).map(e -> e *
+	// e).sum();
+	// }
 
-	  public static <T> List<T> filterList(List<T> list, Predicate<T> p) {
-	    return list.stream().filter(p).collect(Collectors.toList());
-	  }
+	// public static Integer sumSquare(int nb) {
+	// return IntStream.rangeClosed(2, nb).filter(e -> (e & 1) == 0).map(e -> e *
+	// e).sum();
+	// }
 
-	  // public static Integer sumSquare(int nb) {
-	  // return IntStream.rangeClosed(0, nb).filter(e -> e % 2 == 0).map(e -> e * e).sum();
-	  // }
+	public static Integer sumSquare(int nb) {
+		return IntStream.iterate(2, n -> n + 2).limit(nb / 2).map(n -> n * n).sum();
+	}
 
-	  // public static Integer sumSquare(int nb) {
-	  // return IntStream.rangeClosed(2, nb).filter(e -> (e & 1) == 0).map(e -> e * e).sum();
-	  // }
+	public static Map<Boolean, List<Integer>> partitioningList(List<Integer> list, Predicate<Integer> p) {
+		return list.stream().collect(Collectors.partitioningBy(p));
+	}
 
-	  public static Integer sumSquare(int nb) {
-	    return IntStream.iterate(2, n -> n + 2).limit(nb / 2).map(n -> n * n).sum();
-	  }
+	public static <T, U> Map<T, Long> groupByFunction(Stream<U> s, Function<U, T> function) {
+		return s.collect(Collectors.groupingBy(function, Collectors.counting()));
+	}
 
-	  public static Map<Boolean, List<Integer>> partitioningList(List<Integer> list, Predicate<Integer> p) {
-	    return list.stream().collect(Collectors.partitioningBy(p));
-	  }
-
-	  public static <T, U> Map<T, Long> groupByFunction(Stream<U> s, Function<U, T> function) {
-	    return s.collect(Collectors.groupingBy(function, Collectors.counting()));
-	  }
-
-	  public static IntStream primeFactors(int nb) {
-	    return IntStream.range(2, nb).filter(e -> nb % e == 0)
-	        .mapToObj(e -> IntStream.concat(IntStream.of(e), primeFactors(nb / e)))
-	        .findFirst().orElse(IntStream.of(nb));
-	  }
+	public static IntStream primeFactors(int nb) {
+		return IntStream.range(2, nb).filter(e -> nb % e == 0)
+				.mapToObj(e -> IntStream.concat(IntStream.of(e), primeFactors(nb / e))).findFirst()
+				.orElse(IntStream.of(nb));
+	}
 }
--- src/test/java/com/DisplNumIsPalindromFibonacciSort/exemples/ExercicesStreamsTest.java
+++ src/test/java/com/DisplNumIsPalindromFibonacciSort/exemples/ExercicesStreamsTest.java
@@ -29,6 +29,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static com.DisplNumIsPalindromFibonacciSort.exemples.ExercicesStreams.*;
+
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ExercicesStreams.class)
@@ -37,7 +39,7 @@ public class ExercicesStreamsTest {
 	static final PrintStream standardOut = System.out;
 
 	static final ByteArrayOutputStream output = new ByteArrayOutputStream();
-/**/
+
 	@BeforeAll
 	static void setU() throws Exception {
 		System.setOut(new PrintStream(output));
@@ -50,49 +52,31 @@ public class ExercicesStreamsTest {
 
 	@Test
 	void testDisplayNumbers() {
-		ExercicesStreams.displayNumbers(5);
+		displayNumbers(0);
+		assertEquals("", output.toString().trim());
+		displayNumbers(5);
 		assertEquals("0 1 2 3 4", output.toString().trim());
 		output.reset();
 	}
 
-	/*
-	 * @Test
-	 * 
-	 * @DisplayName("testDisplayNumbers") void testDisplayNumbers() {
-	 * displayNumbers(0); assertEquals("", outputStream.toString().trim());
-	 * displayNumbers(5); assertEquals("0 1 2 3 4", outputStream.toString().trim());
-	 * outputStream.reset(); }
-	 */
 	@ParameterizedTest
 	@ValueSource(strings = { "", "a", "radar", "java avaj" })
 	void testIsPalindrome(String word) {
-		assertTrue(ExercicesStreams.isPalindrome(word));
+		assertTrue(isPalindrome(word));
 	}
 
 	@ParameterizedTest
 	@CsvSource(value = { "1,0", "2,1", "3,1", "4,2", "5,3", "6,5", "7,8" })
 	@DisplayName("test Fibo1")
 	void testFibonacci(int in, int out) {
-		assertEquals(ExercicesStreams.fibonacci(in), out);
+		assertEquals(fibonacci(in), out);
 	}
-	/*
-	 * @ParameterizedTest
-	 * 
-	 * @CsvSource(value = {"1,0", "2,1", "3,1", "4,2", "5,3", "6,5", "7,8"})
-	 * 
-	 * @DisplayName("test fibo 1") void testFibonacci(int in, int out) {
-	 * assertEquals(fibonacci(in), out); }
-	 * 
-	 * @Test
-	 * 
-	 * @DisplayName("test fibo 2") void testFibonacci() {
-	 * assertThrows(IllegalArgumentException.class, () -> fibonacci(-1)); }
-	 */
+	
 
 	@Test
 	@DisplayName("test Fibo2")
 	void testFibonacci() {
-		assertThrows(IllegalArgumentException.class, () -> ExercicesStreams.fibonacci(-1));
+		assertThrows(IllegalArgumentException.class, () -> fibonacci(-1));
 	}
 
 	static Stream<Arguments> generateData() {
@@ -106,30 +90,15 @@ public class ExercicesStreamsTest {
 	@ParameterizedTest
 	@MethodSource("generateData")
 	<T> void testSortTab(T[] t, Comparator<T> c, List<T> list) {
-		assertEquals(ExercicesStreams.sortTab(t, c), list);
+		assertEquals(sortTab(t, c), list);
 	}
 
-	/*
-	 * static Stream<Arguments> generateData() { Comparator<Integer> c1 = (n1, n2)
-	 * -> n1 - n2; Comparator<String> c2 = (s1, s2) -> s1.compareTo(s2); return
-	 * Stream.of(Arguments.of(Arrays.asList(5, 2, 4, 3, 1).toArray(), c1,
-	 * Arrays.asList(1, 2, 3, 4, 5)), Arguments.of(Arrays.asList("John", "Alan",
-	 * "Kurt").toArray(), c2, Arrays.asList("Alan", "John", "Kurt"))); }
-	 * 
-	 * @ParameterizedTest
-	 * 
-	 * @MethodSource("generateData") <T> void testSortTab(T[] t, Comparator<T> c,
-	 * List<T> list) { assertEquals(sortTab(t, c), list); }
-	 */
-
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/**/
+	/////////////////////////////////////////////
 
 	@Test
 	@DisplayName("testDisplayAlphabet")
 	void testDisplayAlphabet() {
-		ExercicesStreams.displayAlphabet();
+		displayAlphabet();
 		assertEquals("abcdefghijklmnopqrstuvwxyz", output.toString().trim());
 		output.reset();
 	}
@@ -137,9 +106,9 @@ public class ExercicesStreamsTest {
 	@Test
 	@DisplayName("testMyAbs")
 	void testMyAbs() {
-		assertTrue(ExercicesStreams.myAbs(7) == 7);
-		assertTrue(ExercicesStreams.myAbs(-7) == 7);
-		assertTrue(ExercicesStreams.myAbs(0) == 0);
+		assertTrue(myAbs(7) == 7);
+		assertTrue(myAbs(-7) == 7);
+		assertTrue(myAbs(0) == 0);
 	}
 
 	@Test
@@ -147,13 +116,13 @@ public class ExercicesStreamsTest {
 	void testDisplayTab() {
 		int[] t1 = { 1, 2, 3, 4 };
 
-		ExercicesStreams.displayTab(t1);
+		displayTab(t1);
 		assertEquals("1234", output.toString().trim());
 		output.reset();
 
 		int[] t2 = {};
 
-		ExercicesStreams.displayTab(t2);
+		displayTab(t2);
 		assertEquals("", output.toString().trim());
 		output.reset();
 	}
@@ -162,51 +131,51 @@ public class ExercicesStreamsTest {
 	@DisplayName("testMaxTab")
 	void testMaxTab() {
 		int[] t1 = { 5, 2, 8, -6, 0, 7 };
-		assertTrue(ExercicesStreams.maxTab(t1) == 8);
+		assertTrue(maxTab(t1) == 8);
 
 		int[] t2 = { 1 };
-		assertTrue(ExercicesStreams.maxTab(t2) == 1);
+		assertTrue(maxTab(t2) == 1);
 	}
 
 	@Test
 	@DisplayName("testMean")
 	void testMean() {
-		assertTrue(ExercicesStreams.mean(1) == 1);
-		assertTrue(ExercicesStreams.mean(10, 20, 30) == 20);
-		assertTrue(ExercicesStreams.mean(10, 15, 25, 45) == 23.75);
+		assertTrue(mean(1) == 1);
+		assertTrue(mean(10, 20, 30) == 20);
+		assertTrue(mean(10, 15, 25, 45) == 23.75);
 	}
 
 	@Test
 	@DisplayName("testCountVowels")
 	void testCountVowels() {
-		assertAll("Test countVowels", () -> assertTrue(ExercicesStreams.countVowels("") == 0, ""),
-				() -> assertTrue(ExercicesStreams.countVowels("a") == 1, ""), () -> assertTrue(ExercicesStreams.countVowels("Charles") == 2, ""),
-				() -> assertTrue(ExercicesStreams.countVowels("testeur_") == 3, ""));
+		assertAll("Test countVowels", () -> assertTrue(countVowels("") == 0, ""),
+				() -> assertTrue(countVowels("a") == 1, ""), () -> assertTrue(countVowels("Charles") == 2, ""),
+				() -> assertTrue(countVowels("testeur_") == 3, ""));
 	}
 
 	@Test
 	@DisplayName("testCountConsonants")
 	void testCountConsonants() {
-		assertAll("Test countConsonants", () -> assertTrue(ExercicesStreams.countConsonants("") == 0, ""),
-				() -> assertTrue(ExercicesStreams.countConsonants("z") == 1, ""),
-				() -> assertTrue(ExercicesStreams.countConsonants("Archimède") == 5, ""),
-				() -> assertTrue(ExercicesStreams.countConsonants("testeur_") == 4, ""));
+		assertAll("Test countConsonants", () -> assertTrue(countConsonants("") == 0, ""),
+				() -> assertTrue(countConsonants("z") == 1, ""),
+				() -> assertTrue(countConsonants("Archimède") == 5, ""),
+				() -> assertTrue(countConsonants("testeur_") == 4, ""));
 	}
 
 	@Test
 	@DisplayName("testContainsUpperCase")
 	void testContainsUpperCase() {
-		assertAll("Test containsUpperCase", () -> assertFalse(ExercicesStreams.containsUpperCase(""), ""),
-				() -> assertFalse(ExercicesStreams.containsUpperCase("hello"), ""), () -> assertTrue(ExercicesStreams.containsUpperCase("charleS"), ""),
-				() -> assertTrue(ExercicesStreams.containsUpperCase("TesTeur_"), ""));
+		assertAll("Test containsUpperCase", () -> assertFalse(containsUpperCase(""), ""),
+				() -> assertFalse(containsUpperCase("hello"), ""), () -> assertTrue(containsUpperCase("charleS"), ""),
+				() -> assertTrue(containsUpperCase("TesTeur_"), ""));
 	}
 
 	@Test
 	@DisplayName("testFactorial")
 	void testFactorial() {
-		assertAll("Test factorial", () -> assertTrue(ExercicesStreams.factorial(-1) == 1, ""), () -> assertTrue(ExercicesStreams.factorial(0) == 1, ""),
-				() -> assertTrue(ExercicesStreams.factorial(1) == 1, ""), () -> assertTrue(ExercicesStreams.factorial(2) == 2, ""),
-				() -> assertTrue(ExercicesStreams.factorial(5) == 120, ""), () -> assertTrue(ExercicesStreams.factorial(10) == 3628800, ""));
+		assertAll("Test factorial", () -> assertTrue(factorial(-1) == 1, ""), () -> assertTrue(factorial(0) == 1, ""),
+				() -> assertTrue(factorial(1) == 1, ""), () -> assertTrue(factorial(2) == 2, ""),
+				() -> assertTrue(factorial(5) == 120, ""), () -> assertTrue(factorial(10) == 3628800, ""));
 	}
 
 	@Test
@@ -217,27 +186,27 @@ public class ExercicesStreamsTest {
 		var l3 = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 		assertAll("Test filterList",
-				() -> assertEquals(ExercicesStreams.filterList(l1, s -> s.length() >= 3), Arrays.asList("aaa", "aaaa")),
-				() -> assertEquals(ExercicesStreams.filterList(l2, s -> s.length() <= 2), Arrays.asList("b", "bb", "", "z")),
-				() -> assertEquals(ExercicesStreams.filterList(l3, n -> n % 3 == 0), Arrays.asList(0, 3, 6, 9)));
+				() -> assertEquals(filterList(l1, s -> s.length() >= 3), Arrays.asList("aaa", "aaaa")),
+				() -> assertEquals(filterList(l2, s -> s.length() <= 2), Arrays.asList("b", "bb", "", "z")),
+				() -> assertEquals(filterList(l3, n -> n % 3 == 0), Arrays.asList(0, 3, 6, 9)));
 	}
 
 	@Test
 	@DisplayName("sumSquare")
 	void testsumSquare() {
-		assertAll("Test sumSquare", () -> assertTrue(ExercicesStreams.sumSquare(-1) == 0, ""), () -> assertTrue(ExercicesStreams.sumSquare(0) == 0, ""),
-				() -> assertTrue(ExercicesStreams.sumSquare(1) == 0, ""), () -> assertTrue(ExercicesStreams.sumSquare(2) == 4, ""),
-				() -> assertTrue(ExercicesStreams.sumSquare(3) == 4, ""), () -> assertTrue(ExercicesStreams.sumSquare(6) == 56, ""),
-				() -> assertTrue(ExercicesStreams.sumSquare(10) == 220, ""));
+		assertAll("Test sumSquare", () -> assertTrue(sumSquare(-1) == 0, ""), () -> assertTrue(sumSquare(0) == 0, ""),
+				() -> assertTrue(sumSquare(1) == 0, ""), () -> assertTrue(sumSquare(2) == 4, ""),
+				() -> assertTrue(sumSquare(3) == 4, ""), () -> assertTrue(sumSquare(6) == 56, ""),
+				() -> assertTrue(sumSquare(10) == 220, ""));
 	}
 
 	@Test
 	@DisplayName("partitioningList")
 	void testpartitioningList() {
 		assertAll("Test partitioningList",
-				() -> assertEquals(ExercicesStreams.partitioningList(Arrays.asList(2, 3, 1, 5, 4), e -> e > 2),
+				() -> assertEquals(partitioningList(Arrays.asList(2, 3, 1, 5, 4), e -> e > 2),
 						Map.of(false, Arrays.asList(2, 1), true, Arrays.asList(3, 5, 4))),
-				() -> assertEquals(ExercicesStreams.partitioningList(Arrays.asList(2, 3, 1, 5, 4), e -> e > 0),
+				() -> assertEquals(partitioningList(Arrays.asList(2, 3, 1, 5, 4), e -> e > 0),
 						Map.of(false, Arrays.asList(), true, Arrays.asList(2, 3, 1, 5, 4))));
 	}
 
@@ -250,9 +219,9 @@ public class ExercicesStreamsTest {
 		var s2 = Stream.of("", "a", "ab", "abc");
 
 		assertAll("Test groupByFunction",
-				() -> assertTrue(ExercicesStreams.groupByFunction(s1, String::length).entrySet().stream()
+				() -> assertTrue(groupByFunction(s1, String::length).entrySet().stream()
 						.allMatch(e -> e.getValue().equals(map1.get(e.getKey())))),
-				() -> assertTrue(ExercicesStreams.groupByFunction(s2, s -> s.matches("a")).entrySet().stream()
+				() -> assertTrue(groupByFunction(s2, s -> s.matches("a")).entrySet().stream()
 						.allMatch(e -> e.getValue().equals(map2.get(e.getKey())))));
 	}
 
@@ -260,9 +229,9 @@ public class ExercicesStreamsTest {
 	@DisplayName("testPrimeFactors")
 	void testPrimeFactors() {
 		assertAll("Test primeFactors",
-				() -> assertEquals(ExercicesStreams.primeFactors(8).boxed().collect(Collectors.toList()), IntStream.of(2, 2, 2).boxed().collect(Collectors.toList())),
-				() -> assertEquals(ExercicesStreams.primeFactors(13).boxed().collect(Collectors.toList()), IntStream.of(13).boxed().collect(Collectors.toList())),
-				() -> assertEquals(ExercicesStreams.primeFactors(2500).boxed().collect(Collectors.toList()),
+				() -> assertEquals(primeFactors(8).boxed().collect(Collectors.toList()), IntStream.of(2, 2, 2).boxed().collect(Collectors.toList())),
+				() -> assertEquals(primeFactors(13).boxed().collect(Collectors.toList()), IntStream.of(13).boxed().collect(Collectors.toList())),
+				() -> assertEquals(primeFactors(2500).boxed().collect(Collectors.toList()),
 						IntStream.of(2, 2, 5, 5, 5, 5).boxed().collect(Collectors.toList())));
 	}
 
